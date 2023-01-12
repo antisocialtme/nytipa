e5959e7474812e3d21de9d25882ef3c9a7853b1b
antisocialtme <122570723+antisocialtme@users.noreply.github.com>
2023-01-13 00:19:02 +0300

    sdafsda

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1673556989,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/black_pol-sheet0.png",
+		"images/black_pol-sheet1.png",
+		"images/gg-sheet0.png",
+		"images/gg-sheet1.png",
+		"images/white_pol-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
